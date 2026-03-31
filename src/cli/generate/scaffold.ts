3d68ee09d092a32cf1cc5ea3a801b82ef1d030c7
BosEriko <bos@eriko.ph>
2026-03-31 23:22:26 +0800

    Feature: Add Build Command

--- src/cli/generate/scaffold.ts
+++ src/cli/generate/scaffold.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import pluralize from "pluralize";
-import { generateModel } from "./model.ts";
+import { generateModel } from "./model.js";
 
 export async function generateScaffold(scaffoldName: string) {
   if (!scaffoldName) {
